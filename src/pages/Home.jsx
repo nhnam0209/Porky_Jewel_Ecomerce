3ca92873d5baecc236acb6bbc240605b6708f26f
nhnam3004 <nhnam3004@gmail.com>
2021-11-21 16:21:22 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Categories from '../components/Categories'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import QueueAnim from 'rc-queue-anim';
 import Slider from '../components/Slider'
 
 
@@ -10,13 +9,10 @@ import Slider from '../components/Slider'
 const Home = () => {
     return (
         <div>
-            <QueueAnim>
-                <Navbar/>
-                <Categories/> 
-                <Slider/>
-                <Footer/>
-            </QueueAnim>
-           
+            <Navbar/>
+            <Categories/> 
+            <Slider/>
+            <Footer/>           
         </div>
     )
 }
